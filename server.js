c73129f524d95f07009d89704347e12e5fcc3d46
ChristianMendez1 <ChristianMendez1213@gmail.com>
2023-05-27 12:15:03 -0400

    routes

--- server.js
+++ server.js
@@ -4,11 +4,19 @@ const mongoose = require('mongoose')
 const Resources = require('./models/resources')
 const app = express();
 const port = 3000
+const resourcesController = require('./controllers/resources')
 
-// mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
-// mongoose.connection.once('open', ()=> {
-//     console.log('connected to mongo');
-// });
+// Middleware
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json()); 
+
+// Routes
+app.use('/resources', resourcesController)
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connection.once('open', ()=> {
+    console.log('connected to mongo');
+});
 
 app.listen(port, () => {
     console.log(`Server is listening on, ${port}`)
---
+++ utilities/data.js
@@ -0,0 +1,7 @@
+const resourceData = [
+    {tutorials: 'How to fetch data',
+    jobboards: 'indeed.com',
+    instructions: 'Do stuff'}
+]
+
+module.exports = resourceData;
