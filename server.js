5c9443fb1786bb5de94e57ca5771b977d9352e97
ChristianMendez1 <ChristianMendez1213@gmail.com>
2023-05-27 11:49:04 -0400

    connected

--- server.js
+++ server.js
@@ -2,3 +2,14 @@ const express =require('express')
 require('dotenv').config()
 const mongoose = require('mongoose')
 const Resources = require('./models/resources')
+const app = express();
+const port = 3000
+
+// mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+// mongoose.connection.once('open', ()=> {
+//     console.log('connected to mongo');
+// });
+
+app.listen(port, () => {
+    console.log(`Server is listening on, ${port}`)
+});
