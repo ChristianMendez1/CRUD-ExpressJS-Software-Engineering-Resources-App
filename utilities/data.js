5f84b581741d6d1e0d24c837425d0117691fda78
ChristianMendez1 <ChristianMendez1213@gmail.com>
2023-05-31 15:45:53 -0400

    more seed

--- utilities/data.js
+++ utilities/data.js
@@ -1,9 +1,21 @@
 const resourceData = [
     {tutorialtitles: '3js Tutorial'},
     {tutorialurls:'https://youtu.be/y5CAuAZ7kKM'},
-    {jobboards: 'http://www.indeed.com'},
+    {tutorialtitles: '3js Stars'},
+    {tutorialurls:'https://www.youtube.com/watch?v=_RASm_aOfrE'},
+    {tutorialtitles: 'Nice Portfolio Dashboard'},
+    {tutorialurls:'https://veritystudios.com/'},
+    {jobboards: 'https://www.linkedin.com/jobs/'},
+    {jobboards: 'https://www.ziprecruiter.com/'},
+    {jobboards: 'https://www.dice.com/'},
+    {jobboards: 'https://www.glassdoor.com/'},
+    {jobboards: 'https://hired.com/'},
     {definitiontitles: 'HTML img tag'},
-    {definitions: '<img src="example.jpg" alt="example" width="999" height="999">'}
+    {definitions: '<img src="example.jpg" alt="example" width="999" height="999">'},
+    {definitiontitles: 'Create React'},
+    {definitions: 'npx create-react-app example'},
+    {definitiontitles: 'Github Clone Install'},
+    {definitions: 'npm install'}
 ]
 
 module.exports = resourceData;
